41ba26f6671884851f9513ccda9204a421adeff0
Kipbett <bettkipngenobrian@gmail.com>
2021-02-04 13:07:25 +0300

    Toolbar Commit

--- app/src/main/java/com/example/warehouse/AddItemActivity.java
+++ app/src/main/java/com/example/warehouse/AddItemActivity.java
@@ -1,14 +1,19 @@
 package com.example.warehouse;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
-public class AddItemActivity extends AppCompatActivity {
+public class BookingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_item);
+        setContentView(R.layout.activity_booking);
+
+        Toolbar toolbar = findViewById(R.id.tool_bar_header);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
     }
 }
